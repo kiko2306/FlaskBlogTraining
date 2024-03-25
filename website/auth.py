72e9797895f8e7de52a04acabea1409695f00535
Caroline Budal Arins <carolba@hotmail.com>
2024-03-25 10:28:33 +0000

    database to login

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,21 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth= Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods= ['GET', 'POST'])
 def login():
+    email= request.form.get("email")
+    password= request.form.get("password")
     return render_template("login.html")
 
-@auth.route("/sign_up")
+@auth.route("/sign_up", methods= ['GET', 'POST'])
 def sign_up():
+    email= request.form.get("email")
+    username= request.form.get("username")
+    password1= request.form.get("password1")
+    password2= request.form.get("password2")
     return render_template("sign_up.html")
 
 @auth.route("/logout")
 def logout():
-    return redirect(url_for("views.home")) #when I redirect to a function I can change the url and I won't loose the connection
+    return redirect(url_for("views.home")) # when I redirect to a function I can change the url and I won't loose the connection

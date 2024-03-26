93813c05f58fe7d01d479c6b2599bd0fe7f5f39c
Caroline Budal Arins <carolba@hotmail.com>
2024-03-26 10:34:24 +0000

    ajustes

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 https://www.youtube.com/watch?v=GQcM8wdduLI&list=PLzMcBGfZo4-nK0Pyubp7yIG0RdXp6zklu
 
 ## to install
-pip install Flask
+pip install flask
 pip install Flask-SQLAlchemy
-pip install flask-logind
+pip install flask-login
--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+
 db=SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -13,7 +14,7 @@ def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']= "carol"
     # configure the SQLite database, relative to the app instance folder
-    app.config['SQLALCHEMY_DATABASE_URI']= f"sqlite:///{DB_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{DB_NAME}'
     # initialize the app with the extension
     db.init_app(app)
 
@@ -28,7 +29,7 @@ def create_app():
     create_database(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
+    login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     # getting information about the logged user

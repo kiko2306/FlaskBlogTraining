3550a857f2e3fc3d27971116bbf4e95be170ed61
Miguel1 Teixeira (external) <miguel1.teixeira@kantar.com>
2024-03-26 11:40:54 +0000

    dev

--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,64 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
+auth= Blueprint("auth", __name__)
 
-auth = Blueprint("auth", __name__)
-
-
-@auth.route("/login")
+@auth.route("/login", methods= ['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email= request.form.get("email")
+        password= request.form.get("password")
 
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category = 'success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash("Password is incorrect.", category='error')
+        else:
+            flash("Email does not exist.", category='error')
 
-@auth.route("/sign-up")
+    return render_template("login.html", user=current_user)
+
+@auth.route("/sign_up", methods= ['GET', 'POST'])
 def sign_up():
-    return render_template("signup.html")
+    if request.method == 'POST':
+        email= request.form.get("email")
+        username= request.form.get("username")
+        password1= request.form.get("password1")
+        password2= request.form.get("password2")
 
+        email_exists= User.query.filter_by (email=email).first()
+        username_exists= User.query.filter_by (username=username).first()
+        if email_exists:
+            flash("Email i already in use.", category='error')
+        elif username_exists:
+            flash("Username is alredy in use", category='error')
+        elif password1 != password2:
+            flash("Password don't match!", category='error')
+        elif len(username) < 2:
+            flash("Username is too short.", category='error')
+        elif len(password1) < 6:
+            flash("Password is too short.", category='error')
+        elif len(email) < 4:
+            flash("Email is invalid.", category='error')
+        else:
+            new_user = User(email=email, username=username, password= generate_password_hash(password1, method= 'sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember= True)
+            flash("User created!")
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return redirect(url_for("views.home"))
+    logout_user()
+    return redirect(url_for("views.home")) # when I redirect to a function I can change the url and I won't loose the connection

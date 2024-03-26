1fe4c244c2a1bd241805bec881cf2703d352b160
Miguel1 Teixeira (external) <miguel1.teixeira@kantar.com>
2024-03-26 11:04:35 +0000

    refactoring & bugfix

--- website/__init__.py
+++ website/__init__.py
@@ -4,19 +4,9 @@ from os import path
 from flask_login import LoginManager
 
 
-db=SQLAlchemy()
-DB_NAME = "database.db"
-
-
-#create a flask application
 def create_app():
-    # create the app
-    app=Flask(__name__)
-    app.config['SECRET_KEY']= "carol"
-    # configure the SQLite database, relative to the app instance folder
-    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{DB_NAME}'
-    # initialize the app with the extension
-    db.init_app(app)
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "carol"
 
     from .views import views
     from .auth import auth
@@ -24,22 +14,4 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User
-
-    create_database(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    # getting information about the logged user
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-    
     return app
-
-def create_database(app):
-    if not path.exists("website/" + DB_NAME):
-        db.create_all(app = app)
-        print("Created database!")
